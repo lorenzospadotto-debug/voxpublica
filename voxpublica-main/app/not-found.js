8c46b299030496654ee18e1a42657e925a0f7222
lorenzospadotto-debug <lorenzo.spadotto@gmail.com>
2025-08-31 23:52:41 +0200

    Delete voxpublica-main/app directory

--- voxpublica-main/app/not-found.js
+++
@@ -1,8 +0,0 @@
-export default function NotFound(){
-  return (
-    <div className="container-narrow p-10">
-      <h1 className="text-2xl font-bold">Pagina non trovata</h1>
-      <p className="mt-2">La pagina richiesta non esiste. Torna alla <a className="underline" href="/">home</a>.</p>
-    </div>
-  );
-}
